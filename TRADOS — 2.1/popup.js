e69dceccc05c49de307989eaea13f53892b370be
Lordmanceroot <84021061+Lordmanceroot@users.noreply.github.com>
2021-10-20 18:32:29 +0400

    Add files via upload

---
+++ TRADOS — 2.1/popup.js
@@ -0,0 +1,93 @@
+const popUp = [
+    {
+        "image": "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
+        "title": "Смартфон POCO F3 (8/256gb) черный/ Night Black",
+        "number": "x1",
+        "price": "31 990 ₽",
+    },
+
+    {
+        "image": "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
+        "title": "Смартфон POCO F3 (8/256gb) черный/ Night Black",
+        "number": "x1",
+        "price": "31 990 ₽",
+    },
+
+    {
+        "image": "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
+        "title": "Смартфон POCO F3 (8/256gb) черный/ Night Black",
+        "number": "x1",
+        "price": "31 990 ₽",
+    },
+
+    {
+        "image": "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
+        "title": "Смартфон POCO F3 (8/256gb) черный/ Night Black",
+        "number": "x1",
+        "price": "31 990 ₽",
+    },
+
+    {
+        "image": "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
+        "title": "Смартфон POCO F3 (8/256gb) черный/ Night Black",
+        "number": "x1",
+        "price": "31 990 ₽",
+    },
+
+    {
+        "image": "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
+        "title": "Смартфон POCO F3 (8/256gb) черный/ Night Black",
+        "number": "x1",
+        "price": "31 990 ₽",
+    },
+
+    {
+        "image": "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
+        "title": "Смартфон POCO F3 (8/256gb) черный/ Night Black",
+        "number": "x1",
+        "price": "31 990 ₽",
+    },
+
+    {
+        "image": "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
+        "title": "Смартфон POCO F3 (8/256gb) черный/ Night Black",
+        "number": "x1",
+        "price": "31 990 ₽",
+    },
+
+    {
+        "image": "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
+        "title": "Смартфон POCO F3 (8/256gb) черный/ Night Black",
+        "number": "x1",
+        "price": "31 990 ₽",
+    },
+
+    {
+        "image": "https://miodessa.com/wp-content/uploads/2021/03/poco_f3_12-600x600.jpg",
+        "title": "Смартфон POCO F3 (8/256gb) черный/ Night Black",
+        "number": "x1",
+        "price": "31 990 ₽",
+    },
+
+
+]
+
+popUp.forEach((current) => {
+    const cards = document.querySelector(".popup-list");
+    const template = document.querySelector("#popup-template");
+
+    const newCardPopUp = template.content.cloneNode(true);
+    const popUpBasketImage = newCardPopUp.querySelector(".popup-basket-image");
+    popUpBasketImage.src = current.image;
+    const popUpCardTitle = newCardPopUp.querySelector(".popup-card");
+    popUpCardTitle.textContent = current.title;
+    const popUpBasketNumber = newCardPopUp.querySelector(".basket-number");
+    popUpBasketNumber.textContent = current.number;
+    const popUpBasketPrice = newCardPopUp.querySelector(".basket-price");
+    popUpBasketPrice.textContent = current.price;
+
+
+    cards.appendChild(newCardPopUp);
+
+
+})
